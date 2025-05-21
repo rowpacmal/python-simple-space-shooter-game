1749a335db545973cfd6abcc0ab03979aec8b29f
rowpacmal <rowpacmal@gmail.com>
2025-05-21 20:52:37 +0200

    loaded graphics to the game; player and background stars

--- src/code/main.py
+++ src/code/main.py
@@ -1,16 +1,34 @@
 import pygame
-from os import path
+from os.path import join
+from random import randint
+
 
 # game settings
 BACKGROUND_COLOR = (8, 14, 23)
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
+
 # general setup
 pygame.init()
 pygame.display.set_caption("Space Shooter")
-pygame.display.set_icon(pygame.image.load(path.join("src", "images", "player_a.png")))
+pygame.display.set_icon(pygame.image.load(join("src", "images", "player_a.png")))
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 is_running = True
+
+
+# load assets
+player = pygame.image.load(join("src", "images", "player_a.png")).convert_alpha()
+x, y = 100, 150
+
+# load stars
+star_small = pygame.image.load(join("src", "images", "star_a.png")).convert_alpha()
+star_large = pygame.image.load(join("src", "images", "star_b.png")).convert_alpha()
+star_small.set_alpha(50)
+star_large.set_alpha(50)
+star_positions = [
+    (randint(0, WINDOW_WIDTH - 64), randint(0, WINDOW_HEIGHT - 64)) for _ in range(20)
+]
+
 
 # main game loop
 while is_running:
@@ -21,7 +39,13 @@ while is_running:
 
     # render the game
     display_surface.fill(BACKGROUND_COLOR)
+    for i, pos in enumerate(star_positions):
+        display_surface.blit(star_small if i % 2 == 0 else star_large, pos)
+
+    x += 0.1
+    display_surface.blit(player, (x, y))
     pygame.display.update()
+
 
 # clean up
 pygame.quit()
