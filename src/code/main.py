5d777d69ef75abdc2edaf21e641c3566e861505a
rowpacmal <rowpacmal@gmail.com>
2025-05-21 18:09:20 +0200

    updated main.py; minor refactoring

--- src/code/main.py
+++ src/code/main.py
@@ -1,16 +1,20 @@
 import pygame
+from os import path
+
+# game settings
+BACKGROUND_COLOR = (8, 14, 23)
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
 # general setup
 pygame.init()
-BACKGROUND_COLOR = (8, 14, 23)
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
-pygame.display.set_icon(pygame.image.load("src/images/player_a.png"))
+pygame.display.set_icon(pygame.image.load(path.join("src", "images", "player_a.png")))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 is_running = True
 
+# main game loop
 while is_running:
-    # event loop
+    # event listeners
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
@@ -19,5 +23,6 @@ while is_running:
     display_surface.fill(BACKGROUND_COLOR)
     pygame.display.update()
 
+# clean up
 pygame.quit()
 exit()
